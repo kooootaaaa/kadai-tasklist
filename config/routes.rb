4af621d777685aa44737050a3503394316225b5a
kota.yamada <vivo_test@outlook.jp>
2018-08-16 11:49:46 +0000

    kadai-tasklist Login2

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,16 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  get 'users/new'
-  
+
+  # resources :users, only: [:new, :create]
+  #→動く
+
+  # get 'users/new'
+  # post 'users/create'
+  #→動かない
+
+  get 'users/new', to: 'users#new'
+  post 'users', to: 'users#create'
+
   resources :tasks
 end
